add PTC, log-likelihood. Fixing inc learning

GTM/GTM.py
@@ -88,7 +88,7 @@ class GTM(nn.Module):
             self.X_inc = t
             self.second_old = 0
 
-
+        print("Input dataset size: ", t.shape)
         self.to_be_initialized = False
 
     def responsibility(self, X):
@@ -113,15 +113,33 @@ class GTM(nn.Module):
         # older implementation:
         # return torch.exp(-torch.cdist(self.phi.matmul(self.W), t.to(self.device), p=2, compute_mode= 'donot_use_mm_for_euclid_dist') ** 2).view(self.out_size[0], self.out_size[1])
 
-    def likelihood(self, X, R=None):
+    def log_likelihood(self, X=None):
+        """
+        p(t|x,W,beta) = (beta/2pi)^D/2 * exp(-beta/2 * ||y(W,x) - t||^2)
+        L = sum_n ln(1/K sum_i p(t|x,W,beta))
+        """
+        with torch.no_grad():
+            if self.learning == 'incremental':
+                R = self.R_inc
+                X = self.X_inc
+            else:
+                R = self.responsibility(X)
+            D = X.shape[1]
+            k1 = (self.beta / (2 * torch.pi)).pow(D / 2)
+            k2 = k1 * torch.exp((-self.beta / 2) * torch.cdist(self.phi.matmul(self.W), X.to(self.device), p=2, compute_mode= 'donot_use_mm_for_euclid_dist') ** 2).add_(1e-6)
+
+            return torch.log(k2.sum(axis=0)/self.n_latent_variables).sum()
+
+    def likelihood(self, X=None):
         """
         p(t|x,W,beta) = (beta/2pi)^D/2 * exp(-beta/2 * ||y(W,x) - t||^2)
         ln p = k1 + k2 = D/2 * ln(beta/2pi) + (-beta/2 * ||W.psi - t||^2)
         This leads to the complete-data log likelihood used in EM Eq (10)
         """
-        with torch.no_grad(): # TODO check whether needs grad or not -> no because of EM
-            if R != None and self.learning == 'incremental':
+        with torch.no_grad():
+            if self.learning == 'incremental':
                 R = self.R_inc
+                X = self.X_inc
             else:
                 R = self.responsibility(X)
             D = X.shape[1]
@@ -213,20 +231,18 @@ class GTM(nn.Module):
 
             self.beta = x.numel() / torch.cdist(self.phi.matmul(self.W), x.to(self.device), p=2, compute_mode='donot_use_mm_for_euclid_dist').pow_(2).mul_(R).sum()
 
-            return -self.likelihood(x).div_(n_nodes).item()  # - due to MINIMIZATION
+            return -self.likelihood(x).div(n_nodes).item()  # - due to MINIMIZATION
 
         elif self.learning == 'incremental':
             if self.second_old > second: # new epoch, perform M-step
-                G = torch.diag(self.R_inc.sum(dim=1))
+                G = torch.diag(self.R_inc.sum(dim=1)) # compute G not incrementally
                 self.W = torch.linalg.solve(
                     self.phi.T.matmul(G).matmul(self.phi) + (self.gtm_lr / self.beta) * torch.eye(self.phi.shape[1], device=self.device),
                     self.phi.T.matmul(self.RX_inc))
 
-                # TODO set beta as incremental too (here it's still batched)
-                R = self.responsibility(x)
-                self.beta = x.numel() / torch.cdist(self.phi.matmul(self.W), x.to(self.device), p=2,
-                                                    compute_mode='donot_use_mm_for_euclid_dist').pow_(2).mul_(R).sum()
-
+                # estimate inverse variance beta non incrementally - that would require also a copy of W
+                self.beta = self.X_inc.numel() / torch.cdist(self.phi.matmul(self.W), self.X_inc, p=2,
+                                                    compute_mode='donot_use_mm_for_euclid_dist').pow_(2).mul_(self.R_inc).sum()
                 self.second_old = 0
 
             R_old = self.R_inc[:, first:second].clone()
@@ -235,7 +251,7 @@ class GTM(nn.Module):
             self.RX_inc += (self.R_inc[:, first:second] - R_old).matmul(x)
             self.second_old = second
 
-            return -self.likelihood(self.X_inc, 1).div_(self.R_inc.shape[1]).item()  # - due to MINIMIZATION
+            return -self.log_likelihood(self.X_inc).div(self.R_inc.shape[0]).item()  # - due to MINIMIZATION
 
         else:
             return print("Invalid Learning Method")

experiments/PTC/PTC_conv_gtm.py
@@ -17,35 +17,35 @@ if __name__ == '__main__':
     gc.collect()
     torch.cuda.empty_cache()
 
-    test_name = "discard_2_4_GTM"
+    test_name = "discard_"
     verbose = 1
 
     n_epochs_conv = 200
     n_epochs_readout = 500
     n_epochs_fine_tuning = 500
     n_classes = 2
-    dataset_path = '~/storage/Dataset/'
-    dataset_name = 'MUTAG'
-    n_folds = 3
+    dataset_path = '~/storage/Dataset/PTC_MR'
+    dataset_name = 'PTC_MR'
+    n_folds = 10
     test_epoch = 1
 
     n_units = 30
     lr_conv = 0.00005  # 0.0005
     lr_readout = 0.0005
     lr_fine_tuning = 0.0001
-    weight_decay = 0  # 5e-4
-    drop_prob = 0  # 0.5
+    weight_decay = 5e-4
+    drop_prob = 0.5
     batch_size = 8
 
-    gtm_epoch = 30
-    gtm_grids_dim = (15, 20)
-    gtm_lr = 100 #0.001  # called alpha or lambda in papers
-    gtm_rbf = 8  # this squared equals the amount of rbf basis functions, default = 10
+    gtm_epoch = 100
+    gtm_grids_dim = (18, 20)
+    gtm_lr = 0.0001  # called alpha or lambda in papers
+    gtm_rbf = 12  # this squared equals the amount of rbf basis functions, default = 10
     gtm_learning = 'incremental' # standard or incremental
 
     # early stopping par
-    max_n_epochs_without_improvements = 10
-    early_stopping_threshold = 0.05
+    max_n_epochs_without_improvements = 20
+    early_stopping_threshold = 0.075
     early_stopping_threshold_gtm = 0.05
 
     test_name = test_name + \

impl/binGraphClassifier_GTM_Layer.py
@@ -114,7 +114,7 @@ class modelImplementation_GraphBinClassifier(torch.nn.Module):
                             optimizer=self.conv_optimizer,
                             loader_train=loader_train,
                             loader_test=loader_test,
-                            loader_valid=loader_train,#loader_valid, # TODO SISTEMA!
+                            loader_valid=loader_valid, # TODO SISTEMA!
                             test_epoch=test_epoch,
                             log_file_name="_conv_part_" + test_name,
                             split_id=split_id,
@@ -143,7 +143,7 @@ class modelImplementation_GraphBinClassifier(torch.nn.Module):
             for batch in loader_train:
                 data = batch.to(self.device)
                 _, h_conv, _ = self.model(data, gtm_train=True)
-                h_dataset = torch.cat((h_conv, h_dataset), 0)
+                h_dataset = torch.cat((h_dataset, h_conv), 0)
                 if self.verbose == 1 and split_id == 0:
                     _, reps = torch.unique(data.batch.data, sorted=True, return_counts=True)
                     y_all = np.append(y_all, np.repeat(data.y.detach().cpu().numpy(), reps.cpu().numpy()))
@@ -169,7 +169,7 @@ class modelImplementation_GraphBinClassifier(torch.nn.Module):
         gtm_n_epochs_without_improvements = 0
 
         for epoch in range(n_epochs_gtm):
-            self.model.train()
+            self.model.eval()
 
             epoch_start_time = time.time()
             gtm_losses_batch = np.empty((0, 3))
@@ -207,6 +207,7 @@ class modelImplementation_GraphBinClassifier(torch.nn.Module):
                 fig.suptitle(test_name)
                 colormap = np.array(['navy', 'firebrick'])
                 y_all = y_all.astype(int)
+                # TODO fix this properly with probabilities
                 points = h_conv_1.matmul(torch.linalg.pinv(self.model.gtm1.W)).matmul(self.model.gtm1.matM)
                 axs[0, 0].scatter(points[:, 0].cpu().detach().numpy(), points[:, 1].cpu().detach().numpy(), c=colormap[y_all])
                 axs[0, 0].text(0.01, 0.01, "Beta = "+"{:.4f}".format(self.model.gtm1.beta.item()), verticalalignment='bottom', horizontalalignment='left', transform=axs[0, 0].transAxes)
@@ -221,6 +222,8 @@ class modelImplementation_GraphBinClassifier(torch.nn.Module):
                 axs[0, 2].text(0.01, 0.01, "Beta = " + "{:.4f}".format(self.model.gtm3.beta.item()),
                                verticalalignment='bottom', horizontalalignment='left', transform=axs[0, 2].transAxes)
                 axs[0, 2].set_title("Pre GTM Training - 3rd layer")
+
+            if epoch == 1 and self.verbose == 1 and split_id == 0:
 
                 fig2, axs2 = plt.subplots(2,3, figsize = (10,7))
                 fig2.suptitle(test_name)